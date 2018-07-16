5ee4ae66281200e7249558a9a3ac83d40d034b5f
zhangml <zhangml@nancal.com>
2018-07-16 21:19:29 +0800

    init commit

---
+++ python_1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+n=101
+if n < 100:
+	  print u'小于100'
+elif n > 100:
+      print u'大于100'
+else:
+      print 'xxx'
+	  
+
+n=99
+sum=0
+while n>1:
+	  sum = sum+n
+	  n=n-2
+print "sum:",sum
+print "sumd:%d" %(sum)
+
+
+ 
+number=0
+for x in range(9):
+     number = number+x
+print 'number:',number
